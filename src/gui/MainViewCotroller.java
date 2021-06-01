85a0cac8954e75014c50231042d708c953b2fb53
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-06-01 02:27:26 -0300

    Main view controller

--- src/gui/MainViewCotroller.java
+++ src/gui/MainViewCotroller.java
@@ -8,7 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 
 public class MainViewCotroller implements Initializable {
-
+	
 	@FXML
 	private MenuItem menuItemSeller;
 	
@@ -36,5 +36,4 @@ public class MainViewCotroller implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 	}
-
 }
